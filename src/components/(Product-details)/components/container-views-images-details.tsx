cc30a81812cde093da3b6e90da071d56c0030b7f
Germys G <megustaelmangu47@gmail.com>
2023-01-19 05:31:42 -0400

    conf

--- src/components/(Product-details)/components/container-views-images-details.tsx
+++ src/components/(Product-details)/components/container-views-images-details.tsx
@@ -2,6 +2,7 @@ import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import styles from './css/container-views-images-details.css?inline';
 import ContainerVariantionsDetails from './container-variantions-details';
 import { ContainerHeaderNameBrandProduct } from './container-header-name-brands-product';
+import { ContainerButtonDetails } from './container-button-details';
 export const ContainerViewsIMGDetails = component$(({ props }: any) => {
   useStylesScoped$(styles);
 
@@ -22,18 +23,20 @@ export const ContainerViewsIMGDetails = component$(({ props }: any) => {
         <div class="img-vertical-mobiles-prev">
           <img src={props.images[0]} alt={props.slug} />
         </div>
-
         <div class="container-img-product">
           <img src={props.images[0]} alt="" />
         </div>
       </div>
       <div class="right">
+        <size-w class="size-w-10" />
         <ContainerHeaderNameBrandProduct props={props} />
         {props.variations === undefined ? (
           ''
         ) : (
           <ContainerVariantionsDetails props={props} />
         )}
+
+        <ContainerButtonDetails props={props} />
       </div>
     </div>
   );
