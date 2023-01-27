f85927137685689f08ed4a651dd6eb5bc9aba46c
Germys G <megustaelmangu47@gmail.com>
2023-01-27 13:58:10 -0400

    AGG

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -2,6 +2,7 @@ import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import styles from './carousel.css?inline';
 export default component$(() => {
   useStylesScoped$(styles);
+
   return (
     <>
       <div class="carousel"></div>
