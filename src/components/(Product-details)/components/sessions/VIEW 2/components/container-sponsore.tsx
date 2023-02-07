9db57ddf6dfcbac19ba7423933a06f17d1e835f4
Germys G <megustaelmangu47@gmail.com>
2023-02-07 14:02:29 -0400

    AGG

--- src/components/(Product-details)/components/sessions/VIEW 2/components/container-sponsore.tsx
+++ src/components/(Product-details)/components/sessions/VIEW 2/components/container-sponsore.tsx
@@ -1,14 +1,29 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 
-import styles from './css/container-basic-info.css?inline';
+import styles from './css/container-sponsore.css?inline';
 import { ContainerCardProduct1 } from '~/components/cards/product/product-card-1/product-card-1';
 
 export const ContainerSponsoreProduct = component$(() => {
   useStylesScoped$(styles);
   return (
     <div class="crtrs-bsc">
-      <ContainerCardProduct1 />
-      <p-sr1>Sponsore</p-sr1>
+      <div class="crrts-title">
+        <div class="ofrs">
+          <hs-sr3>Articulos similares en ofertas</hs-sr3>
+          <div class="srs-v">
+            <a-sr1-info>Saber mas</a-sr1-info>
+          </div>
+        </div>
+
+        <p-sr1>Articulos en ofertas que te puede interesar</p-sr1>
+        <div class="scss-md">
+          <a-sr1-info>Saber mas</a-sr1-info>
+        </div>
+      </div>
+      <div class="div-car">
+        {' '}
+        <ContainerCardProduct1 />
+      </div>
     </div>
   );
 });
