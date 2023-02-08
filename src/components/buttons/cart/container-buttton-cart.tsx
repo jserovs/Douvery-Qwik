498172b280add9b0eb392c30f751de80eafd7779
Germys G <megustaelmangu47@gmail.com>
2023-02-08 13:42:48 -0400

    Agg

--- src/components/buttons/cart/container-buttton-cart.tsx
+++ src/components/buttons/cart/container-buttton-cart.tsx
@@ -1,13 +1,11 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import styles from './container-buttton-cart.css?inline';
-import { DouveryCart } from '~/components/icons/cart';
+
 export const ContainerButtonCart = component$(() => {
   useStylesScoped$(styles);
   return (
     <div>
-      <button class="buttonCart">
-        <DouveryCart size="20px" /> Añadir al carrito
-      </button>
+      <button class="buttonCart">Agregar al carrito</button>
     </div>
   );
 });
