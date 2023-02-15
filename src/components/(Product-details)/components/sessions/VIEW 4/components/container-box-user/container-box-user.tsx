10cd9e51ea385b8a92b90d5b4ebb48318a3854fc
Germys G <megustaelmangu47@gmail.com>
2023-02-15 06:38:50 -0400

    AGG

--- src/components/(Product-details)/components/sessions/VIEW 4/components/container-box-user/container-box-user.tsx
+++ src/components/(Product-details)/components/sessions/VIEW 4/components/container-box-user/container-box-user.tsx
@@ -1,14 +1,12 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import style from './container-box-user.css?inline';
+import { DouveryUser } from '~/components/icons/user';
 export const ContainerBoxBser = component$(() => {
   useStylesScoped$(style);
   return (
     <div class="ctr-box-user">
       <div class="crtr-avatar">
-        <img
-          src="https://www.ecured.cu/images/a/a1/Ejemplo_de_Avatar.png"
-          alt="avatar-user"
-        />
+        <DouveryUser size="50" color="#424242" />
       </div>
       <div class="ctr-name-time">
         <hs-sr1>Frederick Sansck</hs-sr1>
