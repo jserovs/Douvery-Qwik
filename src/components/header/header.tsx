b680e4d3ba28dca46aa304a1bd6c4cf5975ae3d1
Germys G <megustaelmangu47@gmail.com>
2023-03-04 18:53:36 +0000

    AGG

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,11 +12,8 @@ import { DouveryCart } from '../icons/cart';
 import { DouveryLogo105X40PX } from '../icons/logo105X40';
 import { fetchSuggestions } from '~/services/fechProduct';
 import { IconsSearch } from '../icons/search';
-<<<<<<< HEAD
 import { fetchUser } from '~/services/auth/token/token';
 
-=======
->>>>>>> master
 interface IState {
   searchInput: string;
   searchResults: string[];
@@ -48,14 +45,11 @@ export default component$(({ is }: any) => {
     };
   });
 
-<<<<<<< HEAD
   const data = fetchUser(
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzN2YyNGNjMjI1MGQzMjhkNmFhMGE3OSIsImlhdCI6MTY3Nzk1MjA5N30.fwXsnqRTJtoe3DM90usCFXoOx9_GJmHRc_CzvwxWAII'
   );
   const name = data;
   console.log(name);
-=======
->>>>>>> master
   return (
     <header>
       <div class="container container-cajas-header ">
@@ -144,11 +138,7 @@ export default component$(({ is }: any) => {
             <div class="cuenta_botom">
               <a class="title-desplg" href="/signin">
                 <span class="wl-hi">¡Hola! </span>
-<<<<<<< HEAD
                 <h4 class="pointer">{name}</h4>
-=======
-                <h4 class="pointer">Registrate</h4>
->>>>>>> master
               </a>
             </div>
           </div>
