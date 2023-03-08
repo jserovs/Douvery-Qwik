60120cea747d5bf8618c1c298e12c168ef39ba6c
Germys G <megustaelmangu47@gmail.com>
2023-03-08 07:48:00 -0400

    AGG

--- src/constants/constants.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from '@builder.io/qwik';
-import { AppState } from './types';
-
-export const APP_STATE = createContext<AppState>('app_state');
-
-export const AUTH_TOKEN = 'authToken';
-
-export const DOUVERY_PUBLIC_URL = 'https://demo.vendure.io/shop-api';
