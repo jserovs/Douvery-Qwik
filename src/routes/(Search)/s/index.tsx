3b9a831714c1b56c830fb15929e8c69a3700597e
Germys G <megustaelmangu47@gmail.com>
2023-03-14 20:00:28 -0400

    AGG

--- src/routes/(Search)/s/index.tsx
+++ src/routes/(Search)/s/index.tsx
@@ -18,6 +18,7 @@ import { DouveryArrowDown } from '~/components/icons/arrow-down';
 
 export default component$(() => {
   useStylesScoped$(styles);
+  const store = useStore({ count: 1 });
 
   const navigate = useNavigate();
 
@@ -33,7 +34,7 @@ export default component$(() => {
     const price = url.searchParams.get('or-p') || 'all';
     const rating = url.searchParams.get('or-r') || 'all';
     const order = url.searchParams.get('or-or') || 'newest';
-    const page = url.searchParams.get('or-p') || 1;
+    const page = url.searchParams.get('or-page') || '1';
 
     return fetchSearchProduct(
       category,
@@ -105,6 +106,19 @@ export default component$(() => {
     ? `&or-p=${url.searchParams.get('or-p')}`
     : '';
 
+  const page = useStore({ setPage: 1 });
+  const pages = useStore({ setPages: 1 });
+  const pageLinks = [];
+  for (let i = 1; i <= pages.setPages; i++) {
+    const isActive = page.setPage === i;
+    pageLinks.push(
+      // eslint-disable-next-line qwik/single-jsx-root
+      <a key={i} href="#" class={isActive ? 'active' : ''} onClick$={() => {}}>
+        {i}
+      </a>
+    );
+  }
+
   return (
     <div class="grid-container">
       <div class="filter-section">
@@ -132,106 +146,118 @@ export default component$(() => {
           </div>
         </div>
         <div class="filter-section-body">
-         <div> <h3>By Category</h3>
-          {category.map((c) => (
-            <div>
-              <label
-                class={
-                  url.searchParams.get('or-c') === c.value
-                    ? 'active-undeline'
-                    : ''
-                }
-              >
-                <input type="checkbox" name="category" value={c.value} />
-                <Link
-                  href={
-                    url.pathname +
-                    `?q=${url.searchParams.get('q')}` +
-                    `&or-c=${c.value}` +
-                    or_p
+          <div>
+            {' '}
+            <h3>By Category</h3>
+            {category.map((c) => (
+              <div>
+                <label
+                  class={
+                    url.searchParams.get('or-c') === c.value
+                      ? 'active-undeline'
+                      : ''
                   }
                 >
-                  {c.name}
-                </Link>
-              </label>
-            </div>
-          ))}</div>
-         <div> <h3>By Price Range</h3>
-          {prices.map((p) => (
-            <li
-              class={
-                url.searchParams.get('or-p') === p.value
-                  ? 'active-undeline'
-                  : ''
-              }
-              key={p.value}
-            >
-              <Link
-                href={
-                  url.pathname +
-                  `?q=${url.searchParams.get('q')}` +
-                  or_c +
-                  `&or-p=${p.value}`
-                }
+                  <input type="checkbox" name="category" value={c.value} />
+                  <Link
+                    href={
+                      url.pathname +
+                      `?q=${url.searchParams.get('q')}` +
+                      `&or-c=${c.value}` +
+                      or_p
+                    }
+                  >
+                    {c.name}
+                  </Link>
+                </label>
+              </div>
+            ))}
+          </div>
+          <div>
+            {' '}
+            <h3>By Price Range</h3>
+            {prices.map((p) => (
+              <li
                 class={
                   url.searchParams.get('or-p') === p.value
                     ? 'active-undeline'
                     : ''
                 }
+                key={p.value}
               >
-                {p.name}
-              </Link>
-            </li>
-          ))}{' '}</div>
-         <div> <h3>By Color</h3>
-          <label>
-            <input type="checkbox" name="color" value="red" />
-            Red
-          </label>
-          <label>
-            <input type="checkbox" name="color" value="blue" />
-            Blue
-          </label>
-          <label>
-            <input type="checkbox" name="color" value="green" />
-            Green
-          </label>
-          <h3>By Size</h3>
-          <label>
-            <input type="checkbox" name="size" value="small" />
-            Small
-          </label>
-          <label>
-            <input type="checkbox" name="size" value="medium" />
-            Medium
-          </label>
-          <label>
-            <input type="checkbox" name="size" value="large" />
-            Large
-          </label></div>
-         <div> <h3>By Ratings</h3>
-          <ul>
-            {ratings.map((r) => (
-              <li key={r.name}>
                 <Link
-                  class="linkdepart"
                   href={
                     url.pathname +
                     `?q=${url.searchParams.get('q')}` +
                     or_c +
-                    `&or-r=${r.rating}`
+                    `&or-p=${p.value}`
+                  }
+                  class={
+                    url.searchParams.get('or-p') === p.value
+                      ? 'active-undeline'
+                      : ''
                   }
                 >
-                  <Stars caption={' & up'} rating={r.rating}></Stars>
+                  {p.name}
                 </Link>
               </li>
-            ))}
-            <li>
-              <Link class="linkdepart">
-                <Stars caption={' & up'} rating={0}></Stars>
-              </Link>
-            </li>
-          </ul></div>
+            ))}{' '}
+          </div>
+          <div>
+            {' '}
+            <h3>By Color</h3>
+            <label>
+              <input type="checkbox" name="color" value="red" />
+              Red
+            </label>
+            <label>
+              <input type="checkbox" name="color" value="blue" />
+              Blue
+            </label>
+            <label>
+              <input type="checkbox" name="color" value="green" />
+              Green
+            </label>
+            <h3>By Size</h3>
+            <label>
+              <input type="checkbox" name="size" value="small" />
+              Small
+            </label>
+            <label>
+              <input type="checkbox" name="size" value="medium" />
+              Medium
+            </label>
+            <label>
+              <input type="checkbox" name="size" value="large" />
+              Large
+            </label>
+          </div>
+          <div>
+            {' '}
+            <h3>By Ratings</h3>
+            <ul>
+              {ratings.map((r) => (
+                <li key={r.name}>
+                  <Link
+                    class="linkdepart"
+                    href={
+                      url.pathname +
+                      `?q=${url.searchParams.get('q')}` +
+                      or_c +
+                      `&or-r=${r.rating}`
+                    }
+                  >
+                    <Stars caption={' & up'} rating={r.rating}></Stars>
+                  </Link>
+                </li>
+              ))}
+              <li>
+                <Link class="linkdepart">
+                  <Stars caption={' & up'} rating={0}></Stars>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
@@ -248,7 +274,11 @@ export default component$(() => {
               </label>
               <select
                 id="slct"
-                 value={selectedValue.selectedValue ? selectedValue.selectedValue : 'toprated'}
+                value={
+                  selectedValue.selectedValue
+                    ? selectedValue.selectedValue
+                    : 'toprated'
+                }
                 onChange$={(event) =>
                   navigate(
                     url.pathname +
@@ -263,7 +293,9 @@ export default component$(() => {
                 <option value="newest">Llegadas más recientes</option>
                 <option value="lowest">Precio: Bajo a Alto</option>
                 <option value="highest">Precio: Alto a Bajo</option>
-                <option value="toprated" selected>Ratings positivos</option>
+                <option value="toprated" selected>
+                  Ratings positivos
+                </option>
               </select>
               <DouveryArrowDown size="14px" />
             </div>
@@ -274,34 +306,60 @@ export default component$(() => {
           onPending={() => <div class="loader"></div>}
           onRejected={(error) => <>Error: {error.message}</>}
           onResolved={(products) => (
-            <ul>
-              {products.length === 0 ? (
-                <p>No hay productos para mostrar.</p>
-              ) : (
-                <ul>
-                  {products.map((product: any) => (
-                    <l i key={product.id}>
-                      <Card1S product={product} />
-                    </l>
-                  ))}
-                </ul>
-              )}
-            </ul>
+            <>
+              {' '}
+              <ul>
+                {products.length === 0 ? (
+                  <p>No hay productos para mostrar.</p>
+                ) : (
+                  <ul>
+                    {products.map((product: any) => (
+                      <l i key={product.id}>
+                        <Card1S product={product} />
+                      </l>
+                    ))}
+                  </ul>
+                )}
+              </ul>
+            </>
           )}
         />
 
         <div class="pagination">
-          <a href="#" class="prev">
+          <button
+            disabled={store.count == 1 || store.count == 0}
+            onClick$={() =>
+              navigate(
+                url.pathname +
+                  `?q=${url.searchParams.get('q')}` +
+                  or_c +
+                  or_p +
+                  `&or-page=${store.count--}`
+              )
+            }
+            class="prev"
+          >
             &#8249; Anterior
-          </a>
+          </button>
           <a href="#" class="active">
-            1
+            {store.count}
           </a>
-          <a href="#">2</a>
-          <a href="#">3</a>
-          <a href="#" class="next">
+
+          <button
+            disabled={store.count === 3}
+            onClick$={() =>
+              navigate(
+                url.pathname +
+                  `?q=${url.searchParams.get('q')}` +
+                  or_c +
+                  or_p +
+                  `&or-page=${store.count++}`
+              )
+            }
+            class="next"
+          >
             Siguiente &#8250;
-          </a>
+          </button>
         </div>
       </div>
     </div>
