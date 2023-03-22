cebfda9bfc8cedd6140d8988bba90695ce86185b
Germys Apple Mac <germysmk99@gmail.com>
2023-03-22 13:45:13 -0400

    AGG

--- src/components/cards/cart/card-3-s/card-3-s.tsx
+++ src/components/cards/cart/card-3-s/card-3-s.tsx
@@ -1,4 +1,4 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { component$, useStore, useStylesScoped$, useVisibleTask$ } from '@builder.io/qwik';
 import styles from './card-3-s.css?inline';
 import { TextCL } from '~/components/use/textCL/textCL';
 import { UsePrice } from '~/components/use/price/price';
@@ -6,7 +6,19 @@ import { UsePrice } from '~/components/use/price/price';
 export const Card3SCART = component$(({ product }: any) => {
   useStylesScoped$(styles);
   const discoun = product.price - product.price * (product.discount / 100);
- 
+ const isDatePassed = useStore({setIsDatePassed:false})
+  useVisibleTask$(() => {
+    const timer = setInterval(() => {
+     const today = new Date();
+    const reminder = new Date(product.reminderDate);
+     if (today > reminder) {
+      isDatePassed.setIsDatePassed=true;
+    }
+    });
+    return () => {
+      clearInterval(timer);
+    };
+  });
   return (
     <div class="container-all">
       <div class="card">
@@ -19,13 +31,16 @@ export const Card3SCART = component$(({ product }: any) => {
             </h2>
           </a>{' '}
           <div class="product-price">
+             <div class={`date ${isDatePassed.setIsDatePassed ? 'date-passed' : 'date-not-passed'}`}>
+          {product.reminderDate}
+        </div>
             {product.discount > 0 ? (
               <>
                 {' '}
                 <UsePrice price={discoun} />
                 <div class="ctr-opa">|</div>
                 <div class="price-t  tach">
-                  {' '}
+                  {' '}``
                   <UsePrice price={product.price} />
                 </div>
               </>
