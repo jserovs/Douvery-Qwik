2d6adeb1b2f8ad483c7b28d9757c8d8c8296d54f
Germys <germisgarcia38@gmail>
2023-03-28 11:27:27 -0400

    AGG

---
+++ src/routes/(Buy)/by/segure/layout.tsx
@@ -0,0 +1,13 @@
+import { component$, Slot } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <>
+      
+      <main>
+        <Slot />
+      </main>
+     
+    </>
+  );
+});
