24bb97982ca9dfa08372ed8edb388b1fa3d805dc
Germys <germisgarcia38@gmail.com>
2023-04-05 10:54:56 -0400

    AGG

---
+++ src/components/cards/buy/pay/card1/product-pay.tsx
@@ -0,0 +1,67 @@
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import styles from './product-pay.css?inline';
+import { TextCL } from '~/components/use/textCL/textCL';
+import { UsePrice } from '~/components/use/price/price';
+import { DouveryCheckMark } from '~/components/icons/checkMark';
+export const Card1PRODUCTPAY = component$(({ product }: any) => {
+  useStylesScoped$(styles);
+  const discoun = product.price - product.price * (product.discount / 100);
+  return (
+    <div class="container-all">
+      <div class="card">
+        <img src={product.images} alt="Product Image" class="product-image" />
+        <div class="product-info">
+          <a href={`/v/${product.slug}/${product.dui}`}>
+            {' '}
+            <h2 class="product-title">
+              <TextCL text={product.name} />{' '}
+            </h2>
+          </a>{' '}
+          {product.price > 998 ? (
+            <div class="ctr-free-shipping">
+              <p>
+                <DouveryCheckMark size="15px" /> Disponible
+              </p>
+              <div class="ctr-opa">|</div>
+              <p>
+                {' '}
+                <DouveryCheckMark size="15px" />
+                Free shipping
+              </p>
+            </div>
+          ) : (
+            <div class="ctr-free-shipping">
+              <p>
+                <DouveryCheckMark size="15px" /> Disponible
+              </p>
+            </div>
+          )}
+          {product.realQuantity >= 5 ? (
+            <></>
+          ) : (
+            <div class="alert-art">
+              <p>¡Queda(n) muy pocos , 5 o menos!</p>
+            </div>
+          )}
+          <div class="product-price">
+            {product.discount > 0 ? (
+              <>
+                {' '}
+                <UsePrice price={discoun} />
+                <div class="ctr-opa">|</div>
+                <div class="price-t  tach">
+                  {' '}
+                  <UsePrice price={product.price} />
+                </div>
+              </>
+            ) : (
+              <>
+                <UsePrice price={product.price} />
+              </>
+            )}{' '}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
