4aaa9dcb2d0389c98321cd77a05e98e84e8e838a
Germys G <megustaelmangu47@gmail.com>
2023-04-05 13:54:00 -0400

    AGGG

---
+++ src/components/(byServices)/Pay/components/method-pay/credit-card/credit-card.tsx
@@ -0,0 +1,5 @@
+import { component$ } from '@builder.io/qwik';
+
+export const CreditCard = component$(() => {
+  return <div>Hello Qwik!</div>;
+});
--- src/components/(byServices)/Pay/sessions/product-pay/product-pay.tsx
+++ src/components/(byServices)/Pay/sessions/product-pay/product-pay.tsx
@@ -14,48 +14,96 @@ export interface IState {
   productResults: Product[];
 }
 
-export const ProductPay = component$(() => {
-  useStylesScoped$(styles);
-  const url = useLocation();
-  const state = useStore<IState>({
-    productResults: [],
-  });
-  useVisibleTask$(async ({ track }) => {
-    track(() => url.pathname);
+export const ProductPay = component$(
+  ({
+    totalAmount,
+    shipping,
+    taxAmount,
+    subTotal,
+    discount,
+    subTotalNoDiscount,
+  }: any) => {
+    useStylesScoped$(styles);
+    const url = useLocation();
+    const state = useStore<IState>({
+      productResults: [],
+    });
+    useVisibleTask$(async ({ track }) => {
+      track(() => url.pathname);
 
-    const controller = new AbortController();
-    state.productResults = await getDataProductCart();
+      const controller = new AbortController();
+      state.productResults = await getDataProductCart();
 
-    return () => {
-      controller.abort();
-    };
-  });
-  return (
-    <div class="container-all">
-      <div class="container-title">
-        <p>Productos a pagar</p>
-        <p>Cantidad</p>
-      </div>
-      <div class="cart-products">
-        {state.productResults.length > 0 ? (
-          state.productResults.map((product) => {
-            return (
-              <>
-                <div class="container-cards-pay" key={product.dui}>
-                  <Card1PRODUCTPAY product={product} />
-                  <div class="container-quantity">{product.quantity}</div>
-                </div>
-              </>
-            );
-          })
-        ) : (
-          <div class="loader"></div>
-        )}
-        <div class="container-edit-cart">
-          {' '}
-          <Link href="/v/cart/">Editar</Link>
+      return () => {
+        controller.abort();
+      };
+    });
+    return (
+      <div class="container-all">
+        <div class="container-title">
+          <p>Productos a pagar</p>
+          <p>Cantidad</p>
+        </div>
+        <div class="cart-products">
+          {state.productResults.length > 0 ? (
+            state.productResults.map((product) => {
+              const taxRate = 0.1; // Por ejemplo, un impuesto del 10%
+              const shippingCost = 5; // Establece un costo de envío fijo, si es necesario
+
+              const subTotalA = state.productResults.reduce(
+                (accumulator, product) => {
+                  return accumulator + product.price * product.quantity;
+                },
+                0
+              );
+              const descounts = state.productResults.reduce(
+                (accumulator, product) => {
+                  const discountAmount =
+                    product.price * (product.discount / 100) * product.quantity;
+                  return accumulator + discountAmount;
+                },
+                0
+              );
+
+              subTotal.setsubTotal = parseFloat(
+                (subTotalA - descounts).toFixed(2)
+              );
+              discount.setDiscount = descounts;
+              subTotalNoDiscount.setsubTotalNoDiscount = subTotalA;
+
+              // Calcular el impuesto
+              const tax = parseFloat(
+                (subTotal.setsubTotal * taxRate).toFixed(2)
+              );
+
+              // Calcular el total incluyendo el impuesto y el costo de envío (si lo hay)
+              const total = parseFloat(
+                (subTotal.setsubTotal + tax + shippingCost).toFixed(2)
+              );
+
+              // Guardar el impuesto, el costo de envío y el total
+              taxAmount.setTaxAmount = tax;
+              shipping.setShipping = shippingCost;
+              totalAmount.setTotalAmount = total;
+              return (
+                <>
+                  <div class="container-cards-pay" key={product.dui}>
+                    <Card1PRODUCTPAY product={product} />
+                    <div class="container-quantity">{product.quantity}</div>
+                  </div>
+                </>
+              );
+            })
+          ) : (
+            <div class="loader"></div>
+          )}
+          <div class="container-edit-cart">
+            {' '}
+            {subTotal.setsubTotal}
+            <Link href="/v/cart/">Editar</Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
