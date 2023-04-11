f08dac8ccd960c23d483399333b262499f841d48
Germys <germisgarcia38@gmail.com>
2023-04-11 16:49:14 -0400

    AGG

--- src/components/buttons/print-button/print-button.tsx
+++ src/components/buttons/print-button/print-button.tsx
@@ -4,7 +4,30 @@ import styles from './print-button.css?inline';
 
 export const PrintButton = component$(({ targetId }: any) => {
   useStylesScoped$(styles);
-  const handlePrint = $(() => {});
+
+  const handlePrint = $(() => {
+    /* const contentToPrint = document.getElementById(targetId).innerHTML;
+
+    const printIframe = document.getElementById('print-iframe');
+    const printDocument = printIframe.contentWindow.document;
+    printDocument.open();
+    printDocument.write('<html><head><title>Douvery Invoice</title>');
+    const stylesheets = document.querySelectorAll(
+      'link[rel="stylesheet"], style'
+    );
+    for (let i = 0; i < stylesheets.length; i++) {
+      printDocument.head.appendChild(stylesheets[i].cloneNode(true));
+    }
+    printDocument.write('</head><body>');
+    printDocument.write(contentToPrint);
+    printDocument.write('</body></html>');
+    printDocument.close();
+    setTimeout(() => {
+      printIframe.contentWindow.focus();
+      printIframe.contentWindow.print();
+    }, 500);
+    */
+  });
   return (
     <>
       <iframe id="print-iframe" style="display:none;">
