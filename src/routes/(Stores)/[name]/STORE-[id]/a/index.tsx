2f969183f4ed9b4772899cba7cad7e6093594413
Germys G <megustaelmangu47@gmail.com>
2023-05-08 07:50:05 -0400

    AGG

--- src/routes/(Stores)/[name]/STORE-[id]/a/index.tsx
+++ src/routes/(Stores)/[name]/STORE-[id]/a/index.tsx
@@ -1,5 +1,25 @@
-import { component$ } from '@builder.io/qwik';
-
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import styles from './index.css?inline';
+import { DouveryIconVerifyBrand } from '~/components/icons/verify';
 export default component$(() => {
-  return <div>Hello Qwik! a</div>;
+  useStylesScoped$(styles);
+  return (
+    <div class="container-all">
+      <h1>Información de la Tienda</h1>
+      <div class="information">
+        <div class="logo">
+          <img src={'/'} alt="" />
+          <a href="#">Douvery</a>
+          <DouveryIconVerifyBrand size="20" />
+        </div>
+        <p>Dirección: Calle Falsa 123, Ciudad, País</p>
+        <p>Teléfono: +00 123 456 789</p>
+        <p>Email: contacto@nombredelatienda.com</p>
+        <p>Phone: Lunes a Viernes de 10:00 a 19:00 hs</p>
+      </div>{' '}
+      <div class="information">
+        <p>Registrada en Douvery: 16 marz 2023</p>
+      </div>
+    </div>
+  );
 });
