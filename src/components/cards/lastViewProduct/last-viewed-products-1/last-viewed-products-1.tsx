166f41bfa6005743fba78fa5f20e8cefb8f3249d
Germys G <megustaelmangu47@gmail.com>
2023-05-16 13:34:30 -0400

    AGG

--- src/components/cards/lastViewProduct/last-viewed-products-1/last-viewed-products-1.tsx
+++ src/components/cards/lastViewProduct/last-viewed-products-1/last-viewed-products-1.tsx
@@ -1,75 +1,77 @@
 import {
-    component$,
-    useStore,
-    useStylesScoped$,
-    useVisibleTask$,
-  } from '@builder.io/qwik';
-  import styles from './last-viewed-products-1.css?inline';
-  import {
-    getDataFavoriteProduct,
-    removeAllProductFavorite,
-  } from '~/services/cart/favorite-product';
-  import type { Product } from '~/utils/types';
-  import { useLocation, useNavigate } from '@builder.io/qwik-city';
-  import { addToCart } from '~/services/cart/cart';
-  import { Card2SCART } from '~/components/cards/cart/card-2-s/card-2-s';
-import { getDataViewedProduct } from '~/services/viewed/viewed';
-import { ContainerCardProduct4 } from '../../product/product-card-4/product-card-4';
-import { Carousel1 } from '~/components/use/carousel/carousel-1/carousel-1';
-  
-  interface IState {
-    searchInput: string;
-    searchResults: Product[];
-    selectedValue: string;
-  }
-  
-  export const CardLastViewedProducts1 = component$(() => {
-    useStylesScoped$(styles);
-    const state = useStore<IState>({
-      searchInput: '',
-      searchResults: [],
-      selectedValue: '',
-    });
-    const isLoading = useStore({ setIsLoading: true });
-  
-    const { url } = useLocation();
-    useVisibleTask$(async ({ track }) => {
-      track(() => url.pathname);
-      const controller = new AbortController();
-  
-      try {
-        state.searchResults = await getDataViewedProduct();
-      } catch (error) {
-        console.error(error);
-      } finally {
-        isLoading.setIsLoading = false;
-      }
-      return () => {
-        controller.abort();
-      };
-    });
-    const nav = useNavigate();
-    return (
-      <>
+  component$,
+  useStore,
+  useStylesScoped$,
+  useVisibleTask$,
+} from '@builder.io/qwik';
+import styles from './last-viewed-products-1.css?inline';
 
-        <div class="cart-future-shop">
-          <h3 class="container-titulo">Marcados como favoritos</h3>
-          <ul class="container-lista">
-            {isLoading.setIsLoading ? (
-              <div class="loader"></div>
-            ) : state.searchResults.length > 0 ? (
-                <Carousel1 styleCard={4} product={  state.searchResults}  />
-                      
-             
-            ) : (
-              <div class="no-product-message">
-                <p>No hay productos en Favorito.</p>
-              </div>
-            )}
-            
-          </ul>
-        </div>
-      </>
-    );
+import type { Product } from '~/utils/types';
+import { useLocation } from '@builder.io/qwik-city';
+
+import { getDataViewedProduct } from '~/services/viewed/viewed';
+
+interface IState {
+  searchInput: string;
+  searchResults: Product[];
+  selectedValue: string;
+}
+
+export const CardLastViewedProducts1 = component$(() => {
+  useStylesScoped$(styles);
+  const state = useStore<IState>({
+    searchInput: '',
+    searchResults: [],
+    selectedValue: '',
   });
-  
+  const isLoading = useStore({ setIsLoading: true });
+
+  const { url } = useLocation();
+  useVisibleTask$(async ({ track }) => {
+    track(() => url.pathname);
+    const controller = new AbortController();
+
+    try {
+      state.searchResults = await getDataViewedProduct();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      isLoading.setIsLoading = false;
+    }
+    return () => {
+      controller.abort();
+    };
+  });
+
+  return (
+    <>
+      <div class="category-card">
+        <div class="category-info">
+          <h1>Ultimos productos vistos</h1>
+        </div>
+        <div class="category-img">
+          {isLoading.setIsLoading ? (
+            <div class="loader"></div>
+          ) : state.searchResults.length > 0 ? (
+            <>
+              {state.searchResults.map((product: any, index: any) => {
+                return (
+                  <div class="image-container" key={index}>
+                    <img src={product.images[0]} alt="" />
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            <div class="no-product-message">
+              <p>No hay productos en Favorito.</p>
+            </div>
+          )}
+        </div>
+        <div class="show-more">
+          <a href={`/s-categorie//`}> Ver todos</a>
+        </div>
+      </div>
+    </>
+  );
+});
--- src/services/viewed/viewed.tsx
+++ src/services/viewed/viewed.tsx
@@ -1,5 +1,5 @@
-import { getCookieData } from "../auth/login/login";
-import { urlServerLocal, urlServerNode } from "../fechProduct";
+import { getCookieData } from '../auth/login/login';
+import { urlServerLocal, urlServerNode } from '../fechProduct';
 
 const OPTIONS_KEY_VIEWED_PRODUCTS = 'V_P';
 
@@ -13,107 +13,129 @@ export function getDataViewedProducts() {
       console.error('Error al decodificar los datos:', error);
     }
   } else {
-    console.warn('No se encontraron datos en localStorage para la clave:', OPTIONS_KEY_VIEWED_PRODUCTS);
+    console.warn(
+      'No se encontraron datos en localStorage para la clave:',
+      OPTIONS_KEY_VIEWED_PRODUCTS
+    );
   }
 }
-export async function addToViewedProducts({dui}: {dui: string}) {
-    console.log('Intentando agregar a la lista de productos vistos:', {dui});
-  
-    // Verifica si la cookie SESSION_D existe
-    const sessionDExists = getCookieData('SESSION_D')
-    if (sessionDExists) {
-      // Si la cookie SESSION_D existe, envía el producto a la API
-      try {
-        const response = await fetch(`${urlServerNode}/api/viewed/productsDui/json`, {
+export async function addToViewedProducts({ dui }: { dui: string }) {
+  console.log('Intentando agregar a la lista de productos vistos:', { dui });
+
+  // Verifica si la cookie SESSION_D existe
+  const sessionDExists = getCookieData('SESSION_D');
+  if (sessionDExists) {
+    // Si la cookie SESSION_D existe, envía el producto a la API
+    try {
+      const response = await fetch(
+        `${urlServerNode}/api/viewed/productsDui/json`,
+        {
           method: 'POST',
-          body: JSON.stringify({dui}),
-          headers: {'Content-Type': 'application/json'},
-        });
-  
+          body: JSON.stringify({ dui }),
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Error al enviar el producto a la API');
+      }
+
+      console.log('Producto enviado a la API:', { dui });
+    } catch (error) {
+      console.error('Error al enviar el producto a la API:', error);
+    }
+  } else {
+    // Si la cookie SESSION_D no existe, guarda el producto en el almacenamiento local
+    const viewed = getDataViewedProducts() || [];
+    const productExists = viewed.some(
+      (product: { dui: string }) => product.dui === dui
+    );
+
+    if (!productExists) {
+      viewed.push({ dui });
+      localStorage.setItem(OPTIONS_KEY_VIEWED_PRODUCTS, JSON.stringify(viewed));
+      console.log('Producto añadido a la lista de productos vistos:', { dui });
+    } else {
+      console.log('El producto ya existe en la lista de productos vistos:', {
+        dui,
+      });
+    }
+  }
+}
+
+export async function transferViewedProductsToAPI() {
+  // Verifica si la cookie SESSION_D existe
+  const sessionDExists = document.cookie
+    .split(';')
+    .some((item) => item.trim().startsWith('SESSION_D='));
+
+  if (sessionDExists) {
+    // Si la cookie SESSION_D existe, obtén los productos del almacenamiento local
+    const viewed = getDataViewedProducts() || [];
+
+    // Envía cada producto a la API
+    for (const product of viewed) {
+      try {
+        const response = await fetch(
+          `${urlServerNode}/api/viewed/productsDui/json`,
+          {
+            method: 'POST',
+            body: JSON.stringify(product),
+            headers: { 'Content-Type': 'application/json' },
+          }
+        );
+
         if (!response.ok) {
           throw new Error('Error al enviar el producto a la API');
         }
-  
-        console.log('Producto enviado a la API:', {dui});
+
+        console.log('Producto enviado a la API:', product);
       } catch (error) {
         console.error('Error al enviar el producto a la API:', error);
       }
-    } else {
-      // Si la cookie SESSION_D no existe, guarda el producto en el almacenamiento local
-      const viewed = getDataViewedProducts() || [];
-      const productExists = viewed.some((product: {dui: string}) => product.dui === dui);
-  
-      if (!productExists) {
-        viewed.push({dui}); 
-        localStorage.setItem(OPTIONS_KEY_VIEWED_PRODUCTS, JSON.stringify(viewed));
-        console.log('Producto añadido a la lista de productos vistos:', {dui});
-      } else {
-        console.log('El producto ya existe en la lista de productos vistos:', {dui});
-      }
     }
+
+    // Borra todos los productos del almacenamiento local
+    removeAllViewedProducts();
   }
-  
-  export async function transferViewedProductsToAPI() {
-    // Verifica si la cookie SESSION_D existe
-    const sessionDExists = document.cookie.split(';').some((item) => item.trim().startsWith('SESSION_D='));
-  
-    if (sessionDExists) {
-      // Si la cookie SESSION_D existe, obtén los productos del almacenamiento local
-      const viewed = getDataViewedProducts() || [];
-  
-      // Envía cada producto a la API
-      for (const product of viewed) {
-        try {
-          const response = await fetch(`${urlServerNode}/api/viewed/productsDui/json`, {
-            method: 'POST',
-            body: JSON.stringify(product),
-            headers: {'Content-Type': 'application/json'},
-          });
-  
-          if (!response.ok) {
-            throw new Error('Error al enviar el producto a la API');
-          }
-  
-          console.log('Producto enviado a la API:', product);
-        } catch (error) {
-          console.error('Error al enviar el producto a la API:', error);
-        }
-      }
-  
-      // Borra todos los productos del almacenamiento local
-      removeAllViewedProducts();
-    }
-  }
-  
-export async function getDataViewedProduct  () {
+}
+
+export async function getDataViewedProduct() {
   const stored = localStorage.getItem(OPTIONS_KEY_VIEWED_PRODUCTS);
   try {
     const settings = JSON.parse(stored as string);
     const response = await fetch(
-  `${urlServerLocal}/api/viewed/productsDui/json`,
-  {
-    method: 'POST',
-    body: JSON.stringify({
-      viewedData: settings
-    }),
-    headers: {
-       'Content-Type': 'application/json',
-    },
-  });
+      `${urlServerLocal}/api/viewed/productsDui/json/`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          limit: 4,
+          viewedData: settings,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
-  if (response.status === 400) {
-  const errorResponse = await response.json();
-  throw new Error(errorResponse.msg);
-  }
+    if (response.status === 400) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.msg);
+    }
     const data = await response.json();
+    console.log(data);
     return data;
-
   } catch (e) {
-    return 'No se encontraron datos en localStorage para la clave: ' + OPTIONS_KEY_VIEWED_PRODUCTS;
+    return (
+      'No se encontraron datos en localStorage para la clave: ' +
+      OPTIONS_KEY_VIEWED_PRODUCTS
+    );
   }
 }
 
 export function removeAllViewedProducts() {
   localStorage.removeItem(OPTIONS_KEY_VIEWED_PRODUCTS);
-  console.log('Todos los elementos han sido eliminados de la lista de productos vistos.');
+  console.log(
+    'Todos los elementos han sido eliminados de la lista de productos vistos.'
+  );
 }
