283f01d2f1054f25cf2b74cfebebaa4b43dde49c
Eucer <megustaelmangu47@gmail.com>
2023-05-16 05:19:17 -0400

    AGG

---
+++ src/components/cards/category/category-1/category-1.tsx
@@ -0,0 +1,22 @@
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import styles from './category-1.css?inline';
+export const Card_Category_1 = component$(({ name, images }: any) => {
+    useStylesScoped$(styles);
+    return <div class="category-card">
+        <div class="category-info">
+            <h1>{name}
+            </h1>
+
+        </div>
+
+        <div class="category-img">
+            {
+                images.map((image: any, index: any) => {
+                    return <img src={image} key={index} />
+                })
+            }
+            ß
+        </div>
+
+    </div>
+});
--- src/components/carousel-all/carousel-all.tsx
+++ src/components/carousel-all/carousel-all.tsx
@@ -1,11 +1,18 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { Card_Category_1 } from '../cards/category/category-1/category-1';
 import styles from './carousel-all.css?inline';
 export default component$(() => {
   useStylesScoped$(styles);
   return (
     <>
-      <div class="carousel">
-        <div class="carousel__container"> </div>
+      <div class="container-all">
+        <Card_Category_1 name="Electrónica Y Accesorios" images={[
+          'https://res.cloudinary.com/douvery/image/upload/v1666290770/samsung%20galaxy%20s22/chzioq3p34ksagujqmfe.webp',
+
+          'https://res.cloudinary.com/douvery/image/upload/v1666290088/iPhone%2014%20Pro%20Max/hsskqod9szag9ohfwv6k.webp'
+
+        ]} />
+
       </div>
     </>
   );
