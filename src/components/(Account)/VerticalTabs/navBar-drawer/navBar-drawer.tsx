53cefd5ea30fa18d03cfce900d7343d110e3137e
Germys G <megustaelmangu47@gmail.com>
2023-05-22 03:54:55 -0400

    AGG

--- src/components/(Account)/VerticalTabs/navBar-drawer/navBar-drawer.tsx
+++ src/components/(Account)/VerticalTabs/navBar-drawer/navBar-drawer.tsx
@@ -1,91 +1,98 @@
-import { $, component$, useOnWindow, useStore, useStylesScoped$ } from '@builder.io/qwik';
+import {
+  $,
+  component$,
+  useOnWindow,
+  useStore,
+  useStylesScoped$,
+} from '@builder.io/qwik';
 import styles from './navBar-drawer.css?inline';
 import { NavBarVertical } from '../navBar-vertical/navBar-vertical';
+import { DouveryMenuHamb } from '~/components/icons/menu';
 export const NavBarDrawer = component$(() => {
-    useStylesScoped$(styles);
-    const store = useStore({
-        isNavOpen: false,
-        isOpen: false,
-        windowWidth: 0,
-    });
+  useStylesScoped$(styles);
+  const store = useStore({
+    isNavOpen: false,
+    isOpen: false,
+    windowWidth: 769,
+  });
 
-    useOnWindow(
-        "load",
-        $(() => {
-            store.windowWidth = window.innerWidth;
-        })
-    );
+  useOnWindow(
+    'load',
+    $(() => {
+      store.windowWidth = window.innerWidth;
+    })
+  );
 
-    useOnWindow(
-        "resize",
-        $(() => {
-            store.windowWidth = window.innerWidth;
-            if (store.windowWidth > 768) {
-                store.isNavOpen = false;
-                store.isOpen = false;
-                document.body.style.overflow = 'auto';
-            }
-        })
-    );
+  useOnWindow(
+    'resize',
+    $(() => {
+      store.windowWidth = window.innerWidth;
+      if (store.windowWidth > 768) {
+        store.isNavOpen = false;
+        store.isOpen = false;
+        document.body.style.overflow = 'auto';
+      }
+    })
+  );
 
-    const handleToggle = $(() => {
-        store.isNavOpen = !store.isNavOpen;
-        store.isOpen = !store.isOpen;
-        document.body.style.overflow = store.isOpen ? 'hidden' : 'auto';
-    });
+  const handleToggle = $(() => {
+    store.isNavOpen = !store.isNavOpen;
+    store.isOpen = !store.isOpen;
+    document.body.style.overflow = store.isOpen ? 'hidden' : 'auto';
+  });
 
+  return (
+    <>
+      <nav>
+        {!store.isNavOpen && store.windowWidth <= 768 ? (
+          <div id="nav-container">
+            <button
+              class="nav-open"
+              aria-label="Open Menu"
+              onClick$={handleToggle}
+            >
+              <DouveryMenuHamb />
+              Ver Menú
+            </button>
+          </div>
+        ) : (
+          ''
+        )}
 
-    return (
-        <>
-            <nav>
-                {/* Open Button */}
-                {!store.isNavOpen && store.windowWidth <= 768 ? (
-                    <button
-                        class="nav-open"
-                        aria-label="Open Menu"
-                        onClick$={handleToggle}
-                    >
-                        abrir
-                    </button>
-                ) : (
-                    ""
-                )}
-                {/* Close Button */}
-                {store.isNavOpen && store.windowWidth <= 768 ? (
-                    <button
-                        class="nav-close"
-                        aria-label="Close Menu"
-                        onClick$={handleToggle}
-                    >
-                        cerrar
-                    </button>
-                ) : (
-                    ""
-                )}
-            </nav>
-            <div class={`sidebar ${store.isOpen ? 'open' : ''}`}>
-                {store.isOpen && store.windowWidth <= 768 ? (
-                    <>
-                        {/* Menu */}
-                        <NavBarVertical />
-                    </>
-                ) : (
-                    ""
-                )}
-            </div>
-            <ul class={store.windowWidth > 768 ? "open" : ""}>
-                {store.windowWidth > 768 ? (
-                    <>
-                        <NavBarVertical />
-
-                    </>
-                ) : (
-                    ""
-                )}
-            </ul>
-            <div class={`overlay ${store.isOpen ? 'open' : ''}`} onClick$={handleToggle}></div>
-
-
-        </>
-    )
+        {store.isNavOpen && store.windowWidth <= 768 ? (
+          <button
+            class="nav-close"
+            aria-label="Close Menu"
+            onClick$={handleToggle}
+          >
+            cerrar
+          </button>
+        ) : (
+          ''
+        )}
+      </nav>
+      <div class={`sidebar ${store.isOpen ? 'open' : ''}`}>
+        {store.isOpen && store.windowWidth <= 768 ? (
+          <>
+            <NavBarVertical />
+          </>
+        ) : (
+          ''
+        )}
+      </div>
+      <ul class={store.windowWidth > 768 ? 'open' : ''}>
+        {store.windowWidth > 768 ? (
+          <div class="container-no-navbar-page-laptop">
+            <NavBarVertical />
+          </div>
+        ) : (
+          ''
+        )}
+      </ul>
+      <div
+        class={`overlay ${store.isOpen ? 'open' : ''}`}
+        onClick$={handleToggle}
+      ></div>
+    </>
+  );
 });
