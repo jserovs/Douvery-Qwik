4aeb3660d10524b6e66c168e8daa81a997f9a0f8
Eucer <megustaelmangu47@gmail.com>
2023-05-23 13:06:17 -0400

    AGG

--- src/components/(Stores)/HorizontalTabsStore/horizontal-tabs-store.tsx
+++ src/components/(Stores)/HorizontalTabsStore/horizontal-tabs-store.tsx
@@ -103,12 +103,22 @@ export const HorizontalTabsStores = component$(() => {
             <div class="header-tabs-wrapper">
               <div class="store-header">
                 <div class="nav">
-
                   < NavBarDrawerStore state={state} />
                   <div class="lgo-movl">
                     <LogoNameStore state={state} />
+                    <a href={'/' +
+                      loc.params.name +
+                      '/' +
+                      'STORE-' +
+                      loc.params.id +
+                      '/a'} aria-label='about button ' class="abount">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 10.75a.75.75 0 0 1 .75.75v5a.75.75 0 0 1-1.5 0v-5a.75.75 0 0 1 .75-.75ZM12 9a1 1 0 1 0 0-2a1 1 0 0 0 0 2Z" /><path fill="currentColor" fill-rule="evenodd" d="M3.25 12a8.75 8.75 0 1 1 17.5 0a8.75 8.75 0 0 1-17.5 0ZM12 4.75a7.25 7.25 0 1 0 0 14.5a7.25 7.25 0 0 0 0-14.5Z" clip-rule="evenodd" /></svg>
+                      <p>About</p>
+                    </a>
+
                   </div>
                 </div>
+
                 <img
                   class="store-banner"
                   src={state.store.design.banners[0]}
@@ -152,20 +162,22 @@ export const HorizontalTabsStores = component$(() => {
                     </li>
                     <li
                       class={`tabs  ${loc.url.pathname.includes('/a/') ? 'active' : ''
-                        }`}
+                        } `}
                     >
-                      <Link
-                        href={
-                          '/' +
-                          loc.params.name +
-                          '/' +
-                          'STORE-' +
-                          loc.params.id +
-                          '/a'
-                        }
-                      >
-                        About store
-                      </Link>
+                      <div class="text-about">
+                        <Link
+                          href={
+                            '/' +
+                            loc.params.name +
+                            '/' +
+                            'STORE-' +
+                            loc.params.id +
+                            '/a'
+                          }
+                        >
+                          About store
+                        </Link>
+                      </div>
                     </li>
                   </ul>
                   <ul class="tab-links">
