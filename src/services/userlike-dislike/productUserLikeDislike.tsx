b90fb1f3a13137df2264abb75ddda9fc79703610
Germys G <megustaelmangu47@gmail.com>
2023-06-11 15:11:27 -0400

    agg

---
+++ src/services/userlike-dislike/productUserLikeDislike.tsx
@@ -0,0 +1,57 @@
+import { urlServerNode } from '../fechProduct';
+
+export async function productUserLikeDislike(
+  userId: string,
+  productDui: string,
+  like: number,
+  controller?: AbortController
+): Promise<any> {
+  const response = await fetch(
+    `
+   ${urlServerNode}/api/user/likeDislikeProduct`,
+    {
+      method: 'POST',
+      signal: controller?.signal,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userId,
+        productDui,
+        like,
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch product');
+  }
+  const results = await response.json();
+  console.log(results);
+  return results;
+}
+
+export async function getProductLikeStatus(
+  userId: string,
+  productDui: string,
+  controller?: AbortController
+): Promise<any> {
+  const response = await fetch(
+    `
+   ${urlServerNode}/api/user/getLikeStatus?userId=${userId}&productDui=${productDui}`,
+    {
+      method: 'GET',
+      signal: controller?.signal,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch product');
+  }
+  const results = await response.json();
+  console.log(results);
+  return results;
+}
