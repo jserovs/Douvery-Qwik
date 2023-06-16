70d2b656b7c0dd5b08b824b32079ac131bf860b0
Germys G <megustaelmangu47@gmail.com>
2023-06-16 05:00:08 -0400

    AGG

--- src/services/userTimestamp/userTimestamp.tsx
+++ src/services/userTimestamp/userTimestamp.tsx
@@ -1,5 +1,5 @@
 import { getCurrentTimestamp } from '~/fuctions/fuctionGetCurrentTimestampUser';
-import { getCookieData } from '../auth/login/login';
+
 import { urlServerNode } from '../fechProduct';
 
 export async function sendUserTimestamp({
@@ -9,30 +9,28 @@ export async function sendUserTimestamp({
   productDui: string;
   userId: string;
 }) {
-  const sessionDExists = getCookieData('SESSION_D');
-
   const timestamp = getCurrentTimestamp();
 
-  if (!sessionDExists) {
-    try {
-      const response = await fetch(
-        `${urlServerNode}/api/product/userTimestamp`,
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            userId,
-            productDui,
-            timestamp,
-          }),
-          headers: { 'Content-Type': 'application/json' },
-        }
-      );
+  try {
+    const response = await fetch(`${urlServerNode}/api/product/userTimestamp`, {
+      method: 'POST',
+      body: JSON.stringify({
+        userId,
+        productDui,
+        timestamp,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-      if (!response.ok) {
-        throw new Error();
-      }
-    } catch (error) {
-      console.error(error);
+    if (!response.ok) {
+      throw new Error();
     }
+
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    console.error('Error:', error);
   }
 }
